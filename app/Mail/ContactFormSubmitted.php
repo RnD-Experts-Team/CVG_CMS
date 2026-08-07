28cad6b9ade10e92a46fa3c9ef463bd0e2319d4d
Aws Aljaramani <166876985+Aws12505@users.noreply.github.com>
2026-08-07 13:29:56 -0400

    Update ContactFormSubmitted.php

--- app/Mail/ContactFormSubmitted.php
+++ app/Mail/ContactFormSubmitted.php
@@ -5,19 +5,38 @@ namespace App\Mail;
 use App\Models\ContactSubmission;
 use Illuminate\Bus\Queueable;
 use Illuminate\Mail\Mailable;
+use Illuminate\Mail\Mailables\Content;
+use Illuminate\Mail\Mailables\Envelope;
 use Illuminate\Queue\SerializesModels;
 
 class ContactFormSubmitted extends Mailable
 {
     use Queueable, SerializesModels;
 
-    public function __construct(public ContactSubmission $submission)
-    {
+    public function __construct(
+        public ContactSubmission $submission
+    ) {
     }
 
-    public function build()
+    public function envelope(): Envelope
     {
-        return $this->subject('New Contact Form Submission — '.$this->submission->full_name)
-            ->view('emails.contact-submission');
+        return new Envelope(
+            subject: 'New Contact Form Submission - ' . $this->submission->full_name,
+        );
+    }
+
+    public function content(): Content
+    {
+        return new Content(
+            view: 'emails.contact-submission',
+            with: [
+                'submission' => $this->submission,
+            ],
+        );
+    }
+
+    public function attachments(): array
+    {
+        return [];
     }
 }
